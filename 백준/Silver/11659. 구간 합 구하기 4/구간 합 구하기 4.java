6af32debabbe1e0041734c26d059f5f6744f4022
swhana <109811519+swhana@users.noreply.github.com>
2022-08-09 10:17:29 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 600 ms, Memory: 60468 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,29 +1,35 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-
-        int[] num = new int[N+1];
-        int[] sum = new int[N+1];
+        int[] num = new int[N + 1];
+        int[] sum = new int[N + 1];
         int[][] ij = new int[M][2];
-        for(int i=1; i<=N; i++) {
-            num[i] = sc.nextInt();
-            sum[i] += sum[i-1] + num[i];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+            sum[i] += sum[i - 1] + num[i];
         }
 
-        for(int i=0; i<M; i++) {
-            ij[i][0] = sc.nextInt();
-            ij[i][1] = sc.nextInt();
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            ij[i][0] = Integer.parseInt(st.nextToken());
+            ij[i][1] = Integer.parseInt(st.nextToken());
         }
         //////////입력부분 끝//////////
-
-        for(int i=0; i<M; i++){
-            System.out.println(sum[ij[i][1]] - sum[ij[i][0]-1]);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < M; i++) {
+            sb.append(sum[ij[i][1]] - sum[ij[i][0] - 1]).append("\n");
         }
-
+        System.out.println(sb);
     }
 }
