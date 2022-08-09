3a9770de8c2048743849bd94fe80d77b3526c68f
swhana <109811519+swhana@users.noreply.github.com>
2022-08-09 10:34:03 +0900

    [Silver IV] Title: 동전 0, Time: 112 ms, Memory: 12888 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        int[] coin = new int[N];
+        for(int i=0; i<N; i++){
+            coin[i] = sc.nextInt();
+        }
+        //////////입력부분 종료//////////
+
+        int total = 0;
+        for(int i=N-1; i>=0; i--){
+            total+=K/coin[i];
+            K = K%coin[i];
+        }
+
+        System.out.println(total);
+    }
+}
