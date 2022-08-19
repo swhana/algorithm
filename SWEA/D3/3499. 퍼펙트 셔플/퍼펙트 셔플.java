aeb5a6c7f1ccfb32023f20d1137ab4cd20bdcf5e
swhana <109811519+swhana@users.noreply.github.com>
2022-08-19 11:59:19 +0900

    [D3] Title: 퍼펙트 셔플, Time: 185 ms, Memory: 35,632 KB -BaekjoonHub

--- SWEA/D3/3499. 퍼펙트 셔플/퍼펙트 셔플.java
+++ SWEA/D3/3499. 퍼펙트 셔플/퍼펙트 셔플.java
@@ -11,10 +11,13 @@ public class Solution {
 		for (int k = 1; k <= tc; k++) {
 			int N = sc.nextInt();
 
+			// 앞 절반 받을 큐
 			Queue<String> queue1 = new LinkedList();
+			// 뒤 절반 받을 큐
 			Queue<String> queue2 = new LinkedList();
 
 			for (int i = 0; i < N; i++) {
+				// 전체 개수가 홀수면 앞쪽 큐가 1개 더많도록
 				if (i < (N + 1) / 2)
 					queue1.offer(sc.next());
 				else
@@ -25,6 +28,7 @@ public class Solution {
 
 			sb.append("#" + k + " ");
 			for (int i = 0; i < N; i++) {
+				// 하나씩 번갈아가면서 추출
 				if (i % 2 == 0)
 					sb.append(queue1.poll() + " ");
 				else
