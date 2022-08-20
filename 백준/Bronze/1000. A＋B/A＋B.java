d6a550032479188f0b36bb0351035d133d1e011e
swhana <109811519+swhana@users.noreply.github.com>
2022-08-20 13:23:10 +0900

    [Bronze V] Title: A+B, Time: 112 ms, Memory: 12856 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);	//scanner 객체 sc 생성
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println(a+b);
+	}
+}
+
