97550cf110424b31498d0ed3ce9547ffe60dff35
swhana <109811519+swhana@users.noreply.github.com>
2022-08-26 14:03:58 +0900

    [Gold V] Title: AC, Time: 888 ms, Memory: 98772 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.java
+++ 백준/Gold/5430. AC/AC.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Deque;
 import java.util.LinkedList;
 import java.util.StringTokenizer;
@@ -8,10 +6,10 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
         int tc = Integer.parseInt(br.readLine());
-
 
         for (int k = 1; k <= tc; k++) {
             boolean flag = true;    //에러판별기
@@ -28,49 +26,60 @@ public class Main {
                 deq.offerLast(Integer.parseInt(st.nextToken()));
             }
 
+            /*문자열을 계속 뒤집는건 시간초과가 나기 때문에
+             D가 입력될 때나 문자열 출력시에 R갯수를 체크해
+             문자열이 정방향인지 역방향인지만 확인하면 된다*/
             int Rcount = 0;
             for (int i = 0; i < func.length(); i++) {
                 if (func.charAt(i) == 'R') {
                     Rcount++;
                 } else if (func.charAt(i) == 'D') {
-                    if (Rcount % 2 == 0) {     //R갯수가 짝수면 정상인 상태, 홀수면 아닌상태
-                        if (deq.peekFirst() == null) {
+                    if (Rcount % 2 == 0) {     //R갯수가 짝수면 정방향, 홀수면 역방향
+                        //정방향 체크
+                        if (deq.peekFirst() == null) { //에러 플래그가 false면 error출력
                             flag = false;
                             break;
                         } else
                             deq.pollFirst();
                     } else {
-                        if (deq.peekLast() == null) {
+                        //역방향 체크
+                        if (deq.peekLast() == null) { //에러 플래그가 false면 error출력
                             flag = false;
                             break;
                         } else
                             deq.pollLast();
                     }
-
                 }
             }
-            StringBuilder sb = new StringBuilder();
+
+            //출력부
             if (flag) {
-                sb.append("[");
+                bw.write("[");
+                //정방향일 경우
                 if (Rcount % 2 == 0) {
                     while (!deq.isEmpty()) {
-                        sb.append(deq.pollFirst());
+                        bw.write(String.valueOf(deq.pollFirst()));
                         if (deq.size() != 0)
-                            sb.append(",");
+                            bw.write(",");
                     }
-                    sb.append("]");
-                    System.out.println(sb);
-                } else {
+                    bw.write("]");
+                }
+                //역방향일 경우
+                else {
                     while (!deq.isEmpty()) {
-                        sb.append(deq.pollLast());
+                        bw.write(String.valueOf(deq.pollLast()));
                         if (deq.size() != 0)
-                            sb.append(",");
+                            bw.write(",");
                     }
-                    sb.append("]");
-                    System.out.println(sb);
+                    bw.write("]");
                 }
             } else
-                System.out.println("error");
+                bw.write("error");
+            bw.write("\n");
+
+            bw.flush();
         }
+        br.close();
+        bw.close();
     }
 }
