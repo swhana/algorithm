8a2e00d73c91d95913be650313733ff2f6208481
swhana <109811519+swhana@users.noreply.github.com>
2022-09-14 17:30:03 +0900

    [Silver III] Title: 바이러스, Time: 76 ms, Memory: 11704 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -13,6 +13,7 @@ public class Main {
         int N = Integer.parseInt(br.readLine());    //컴퓨터의 수(노드 수)
         int M = Integer.parseInt(br.readLine());    //연결된 컴퓨터 쌍의 수(간선 수)
 
+        //알아보기 쉽도록...(1번방부터 시작)
         com = new LinkedList[N + 1];
         visited = new boolean[N + 1];
 
@@ -26,13 +27,18 @@ public class Main {
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
+            //연결된 간선 처리
             com[a].add(b);
             com[b].add(a);
         }
 
+        //1번 컴퓨터와 연결된 모든 컴퓨터를 찾기
         dfs(1);
+
+        //바이러스에 걸린 컴퓨터 수를 저장할 변수
         int cnt = 0;
         for (int i = 2; i < N + 1; i++) {   //1번 컴퓨터 제외
+            //1번 컴퓨터와 연결되어 있다면(dfs(1)을 통해 방문한 적이 있다면)
             if (visited[i]) {
                 cnt++;
             }
@@ -42,8 +48,10 @@ public class Main {
     }
 
     static void dfs(int idx) {
-        visited[idx] = true;
+        visited[idx] = true;    //방문 처리
+        //com[idx]의 모든 자식 노드 i 방문
         for (int i : com[idx]) {
+            //방문한 적이 없다면
             if (visited[i] == false) {
                 visited[i] = true;
                 dfs(i);
