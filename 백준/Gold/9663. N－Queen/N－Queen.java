5d35d01337f8f2cf0b7ef6650d3148ffacfd7b85
swhana <109811519+swhana@users.noreply.github.com>
2022-09-22 12:06:46 +0900

    [Gold IV] Title: N-Queen, Time: 4512 ms, Memory: 13440 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Main {
+    static int[] queen;   //queen[행] = 열 이런식으로 저장하면 2차원배열이 아니어도 괜찮다
+    static int N;
+    static int cnt; //가짓수 세기
+
+    private static void dfs(int depth) {
+        //마지막 열까지 다 놓았을 경우
+        if (depth == N) {
+            cnt++;
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            boolean flag = false;
+            //i번째 열에 퀸을 놓고싶다
+            //이전 행들을 탐색
+            for (int j = 0; j < depth; j++) {
+                int diff = depth - j;   //현재 행과 거리차이
+                if (queen[j] == i || queen[j] == i - diff || queen[j] == i + diff) {    //뭐 하나라도 걸리면 나가
+                    flag = true;
+                    break;
+                }
+            }
+
+            //하나도 걸리는게 없었다면
+            if (!flag) {
+                queen[depth] = i;   //퀸 놓기
+                dfs(depth + 1);     //다음 열로 넘어가기
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+        queen = new int[N];
+        cnt = 0;
+
+        dfs(0);
+
+        System.out.println(cnt);
+
+
+    }
+}
