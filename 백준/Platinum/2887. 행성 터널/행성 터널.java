84a5b3580dbfd383eb49de84af677e8c131ee143
swhana <109811519+swhana@users.noreply.github.com>
2022-09-30 16:24:41 +0900

    [Platinum V] Title: 행성 터널, Time: 1564 ms, Memory: 77196 KB -BaekjoonHub

--- 백준/Platinum/2887. 행성 터널/행성 터널.java
+++ 백준/Platinum/2887. 행성 터널/행성 터널.java
@@ -8,11 +8,11 @@
     ... N-2번째에서 가장 가까운 정점은 N-1번째가 된다
     총 N-1개의 간선이 나오게 된다
     Edge(i, i+1, dist)
-    
-
-
  */
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
@@ -20,7 +20,7 @@ public class Main {
     static int[] p;
     static int[] rank;
 
-    //각 행성별 정보를 담자
+    //각 행성별 정보를 담자(정점)
     static class Star {
         int x;
         int y;
@@ -35,6 +35,21 @@ public class Main {
         }
     }
 
+    //간선
+    static class Edge {
+        int st; //시작정점의 index
+        int ed; //도착정점의 index
+        int dist;    //간선의 길이
+
+        //node[st]와 node[ed] 사이의 간선
+        public Edge(int st, int ed, int dist) {
+            this.st = st;
+            this.ed = ed;
+            this.dist = dist;
+        }
+    }
+
+    //Kruskal Algorithm을 위한 setting
     //i번째 별이 속한 그룹의 대표자를 찾아주세요
     public static int find(int x) {
         if (x != p[x])
@@ -54,45 +69,35 @@ public class Main {
             if (rank[px] == rank[py])
                 rank[py]++;
         }
-
     }
 
 
-    static class Edge {
-        int st; //시작정점의 index
-        int ed; //도착정점의 index
-        int dist;    //간선의 길이
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stt;
 
-        //node[st]와 node[ed] 사이의 간선
-        public Edge(int st, int ed, int dist) {
-            this.st = st;
-            this.ed = ed;
-            this.dist = dist;
-        }
-    }
+        int N = Integer.parseInt(br.readLine());   //행성의 개수
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        //Kruskal Algorithm을 위한 준비
+        p = new int[N];     //대표자를 담는 배열
+        rank = new int[N];  //트리의 rank비교
 
-        int N = sc.nextInt();   //행성의 개수
+        Star[] stars = new Star[N];                 //정점을 넣을 배열
+        ArrayList<Edge> fin = new ArrayList<>();    //간선을 넣을 list
 
-
-        p = new int[N];
-        rank = new int[N];
-
-        Star[] stars = new Star[N];
-
+        //초기화 파트
         for (int i = 0; i < N; i++) {
+            stt = new StringTokenizer(br.readLine());
             p[i] = i;
             rank[i] = 0;
-            int x = sc.nextInt();
-            int y = sc.nextInt();
-            int z = sc.nextInt();
-            stars[i] = new Star(i, x, y, z);
+            int x = Integer.parseInt(stt.nextToken());
+            int y = Integer.parseInt(stt.nextToken());
+            int z = Integer.parseInt(stt.nextToken());
+            //행성번호를 따로 기록해두지 않으면 정렬할때마다 순서가 뒤죽박죽이 된다
+            stars[i] = new Star(i, x, y, z);        //i: 행성번호, x, y, z : 좌표
         }
 
-        ArrayList<Edge> fin = new ArrayList<>();
-
+        //x좌표를 기준으로 정렬
         Arrays.sort(stars, new Comparator<Star>() {
             @Override
             public int compare(Star o1, Star o2) {
@@ -100,12 +105,12 @@ public class Main {
             }
         });
 
-
         for (int i = 0; i < N - 1; i++) {
-            int dist = Math.abs(stars[i].x - stars[i + 1].x);       //x좌표 차이가 최단거리
-            fin.add(new Edge(stars[i].idx, stars[i + 1].idx, dist));
+            int dist = Math.abs(stars[i].x - stars[i + 1].x);           //i행성에서 가장 가까운 정점과의 x좌표 거리
+            fin.add(new Edge(stars[i].idx, stars[i + 1].idx, dist));    //i행성에서 x좌표상 가장 가까운 정점과의 간선을 리스트에 입력
         }
 
+        //y좌표를 기준으로 정렬
         Arrays.sort(stars, new Comparator<Star>() {
             @Override
             public int compare(Star o1, Star o2) {
@@ -113,13 +118,12 @@ public class Main {
             }
         });
 
-
         for (int i = 0; i < N - 1; i++) {
-            int dist = Math.abs(stars[i].y - stars[i + 1].y);       //y좌표 차이가 최단거리
-            fin.add(new Edge(stars[i].idx, stars[i + 1].idx, dist));
+            int dist = Math.abs(stars[i].y - stars[i + 1].y);           //i행성에서 가장 가까운 정점과의 y좌표 거리
+            fin.add(new Edge(stars[i].idx, stars[i + 1].idx, dist));    //i행성에서 y좌표상 가장 가까운 정점과의 간선을 리스트에 입력
         }
 
-
+        //z좌표를 기준으로 정렬
         Arrays.sort(stars, new Comparator<Star>() {
             @Override
             public int compare(Star o1, Star o2) {
@@ -129,11 +133,11 @@ public class Main {
 
 
         for (int i = 0; i < N - 1; i++) {
-            int dist = Math.abs(stars[i].z - stars[i + 1].z);       //z좌표 차이가 최단거리
-            fin.add(new Edge(stars[i].idx, stars[i + 1].idx, dist));
+            int dist = Math.abs(stars[i].z - stars[i + 1].z);            //i행성에서 가장 가까운 정점과의 z좌표 거리
+            fin.add(new Edge(stars[i].idx, stars[i + 1].idx, dist));     //i행성에서 z좌표상 가장 가까운 정점과의 간선을 리스트에 입력
         }
 
-
+        //총 3(N-1)개의 간선이 리스트에 들어갔고, 이걸 다시 dist(거리)순으로 정렬
         Collections.sort(fin, new Comparator<Edge>() {
             @Override
             public int compare(Edge o1, Edge o2) {
@@ -141,20 +145,20 @@ public class Main {
             }
         });
 
-        int res = 0;
-        int cnt = 0;
+        int res = 0;        //결과값을 담을 변수
+        int cnt = 0;        //시행횟수
         for (int i = 0; i < fin.size(); i++) {
-            Edge e = fin.get(i);
+            Edge e = fin.get(i);    //가장 작은 dist를 가진 간선부터 추출
 
             int st = e.st;
             int ed = e.ed;
 
-            if (find(st) == find(ed)) continue;
+            if (find(st) == find(ed)) continue; //이미 한번 시행한 간선일 경우 더이상 처리하지 않음(예: 이미 x좌표로 최단거리를 구했으면 y좌표나 z좌표는 무시)
 
             union(st, ed);
-            res += e.dist;
+            res += e.dist;              //최단거리를 결과값에 더해줌
             cnt++;
-            if (cnt == N - 1) break;
+            if (cnt == N - 1) break;    //N개의 정점에 대해 N-1개의 간선을 얻으면 거기서 MST완성
         }
 
         System.out.println(res);
