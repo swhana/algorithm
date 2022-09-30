41b5b92de67344642ebb06db41b007d1b59ef017
swhana <109811519+swhana@users.noreply.github.com>
2022-09-30 10:03:10 +0900

    [Gold IV] Title: 사이클 게임, Time: 684 ms, Memory: 136708 KB -BaekjoonHub

--- 백준/Gold/20040. 사이클 게임/사이클 게임.java
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.java
@@ -1,4 +1,7 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     static int[] p; //대표자를 저장해주는 배열
@@ -12,33 +15,29 @@ public class Main {
         return p[x];    //x==p[x]여도 아니어도 p[x]를 리턴하면 된다
     }
 
-    //정점 x와 y를 연결하자
-    //이 문제는 간선간의 관계는 별로 중요하지 않기 때문에 따로 처리는 안함
-    public static void union(int x, int y) {
-        int px = find(x);   //조상을 데려오자
-        int py = find(y);
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-    }
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());   //정점의 개수
+        int M = Integer.parseInt(st.nextToken());  //게임 시행 횟수 = 간선의 갯수
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int N = sc.nextInt();   //정점의 개수
-        int M = sc.nextInt();   //게임 시행 횟수 = 간선의 갯수
-
+        //배열 초기화
         p = new int[N];
         rank = new int[N];
         for (int i = 0; i < N; i++) {
-            p[i] = i;   //초기화
+            p[i] = i;
             rank[i] = 0;
         }
 
-
         for (int i = 0; i < M; i++) {
-            int x = sc.nextInt();
-            int y = sc.nextInt();
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
 
+            //여기서 부터는 Union처리
             int px = find(x);
             int py = find(y);
             //두 입력값의 부모(대표자)가 같다면
