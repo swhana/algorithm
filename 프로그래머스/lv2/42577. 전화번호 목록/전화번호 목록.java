523c1ff4b89c4a454b4190ab0cc44ca8b738626f
swhana <109811519+swhana@users.noreply.github.com>
2023-02-23 21:51:36 +0900

    [level 2] Title: 전화번호 목록, Time: 2332.27 ms, Memory: 408 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
@@ -3,16 +3,16 @@ import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
-        HashMap<String, Integer> map = new HashMap<String, Integer>();
+        HashSet<String> set = new HashSet<String>();
         
         for(int i=0; i<phone_book.length; i++){
             for(int j=0; j<phone_book[i].length(); j++){
-                map.put(phone_book[i].substring(0, j), 0);
+                set.add(phone_book[i].substring(0, j));
             }
         }
         
         for(int i=0; i<phone_book.length; i++){
-            if(map.containsKey(phone_book[i])) return false;
+            if(set.contains(phone_book[i])) return false;
         }
         
         return answer;
