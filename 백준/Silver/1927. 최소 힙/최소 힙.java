160592735295e4923de1f0fab7424d92c1df08ba
swhana <109811519+swhana@users.noreply.github.com>
2023-04-17 17:22:13 +0900

    [Silver II] Title: 최소 힙, Time: 856 ms, Memory: 101612 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        int N = sc.nextInt(); //연산의 개수
+
+        //오름차순 정렬
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+
+        for(int i=0; i<N; i++){
+            int input = sc.nextInt();
+
+            if(input == 0) {
+                if(q.isEmpty()) sb.append(0).append("\n");
+                else sb.append(q.poll()).append("\n");
+            } else {
+                q.add(input);
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
