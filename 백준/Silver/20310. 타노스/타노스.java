8f5121a457103488d1a6cf70cada5541d837529a
Han Sang Won <109811519+swhana@users.noreply.github.com>
2023-05-09 00:05:46 +0900

    Update 타노스.java
    부분점수 맞았던거 수정

--- 백준/Silver/20310. 타노스/타노스.java
+++ 백준/Silver/20310. 타노스/타노스.java
@@ -5,21 +5,48 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		String input = sc.nextLine();
+
+		int cntZero = 0;
+		int cntOne = 0;
+		for (int i = 0; i < input.length(); i++) {
+			if (input.charAt(i) == '0')
+				cntZero++;
+			else
+				cntOne++;
+		}
+
+		int startZero = input.length() - 1;
+		int dcntZero = cntZero / 2;
 		
-		long cntZero = 0;
-		long cntOne = 0;
-		for(int i=0; i<input.length(); i++) {
-			if(input.charAt(i) == '0') cntZero++;
-			else cntOne++;
+		String[] arr = input.split("");
+		
+		//맨 오른쪽 끝부터 0을 절반 지워나간다.
+		while(dcntZero > 0) {
+			if(arr[startZero].equals("0")) {
+				dcntZero--;
+				arr[startZero] = "";
+			}
+			startZero--;
 		}
 		
+		int startOne = 0;
+		int dcntOne = cntOne / 2;
+		
+		//맨 왼쪽 끝부터 1을 절반 지워나간다.
+		while(dcntOne > 0) {
+			if(arr[startOne].equals("1")) {
+				dcntOne--;
+				arr[startOne] = "";
+			}
+			startOne++;
+		}
+		
+		//이렇게 하면 사전순으로 정렬된 문자열이 생성된다.
 		StringBuilder sb = new StringBuilder();
-		for(int i=0; i<cntZero/2; i++) sb.append("0");
-		for(int i=0; i<cntOne/2; i++) sb.append("1");
+		for(String s : arr) sb.append(s);
 		
 		System.out.println(sb);
 	}
-
 }
