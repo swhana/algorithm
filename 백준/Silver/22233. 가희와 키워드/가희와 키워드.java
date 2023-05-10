b520e5952043f8a5d9b4ab981c43d6232b93e14b
Han Sang Won <109811519+swhana@users.noreply.github.com>
2023-05-11 00:03:08 +0900

    [Silver II] Title: 가희와 키워드, Time: 1532 ms, Memory: 248796 KB -BaekjoonHub

--- 백준/Silver/22233. 가희와 키워드/가희와 키워드.java
+++ 백준/Silver/22233. 가희와 키워드/가희와 키워드.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
 		
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
@@ -26,7 +27,8 @@ public class Main {
 					total--; //키워드를 제거
 				}
 			}
-			System.out.println(total);
+			sb.append(total).append("\n");
 		}
+		System.out.print(sb);
 	}
 }
