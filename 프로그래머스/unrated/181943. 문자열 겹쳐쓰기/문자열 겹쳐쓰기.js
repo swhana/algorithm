d0ff6ed0c7d797fe1ca6224a5ab213ba90c2c453
Han Sang Won <109811519+swhana@users.noreply.github.com>
2023-05-20 01:41:16 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
@@ -0,0 +1,14 @@
+function solution(my_string, overwrite_string, s) {
+    var answer = '';
+    
+    for(let i=0; i<s; i++)
+        answer += my_string[i];
+    
+    
+    answer += overwrite_string;
+    
+    for(let i=s+overwrite_string.length; i<my_string.length; i++)
+        answer += my_string[i];
+    
+    return answer;
+}
