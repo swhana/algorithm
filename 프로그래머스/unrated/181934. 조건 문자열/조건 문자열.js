a9fbc065573057f6cd6926372e23c320dd08362e
Han Sang Won <109811519+swhana@users.noreply.github.com>
2023-05-23 22:53:01 +0900

    [unrated] Title: 조건 문자열, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.js
@@ -0,0 +1,10 @@
+function solution(ineq, eq, n, m) {
+    var answer = 0;
+    
+    if(ineq == ">" && eq == "=") return n >= m ? 1 : 0;
+    else if(ineq == "<" && eq == "=") return n <= m ? 1 : 0;
+    else if(ineq == ">" && eq == "!") return n > m ? 1 : 0;
+    else return n < m ? 1 : 0;
+    
+    return answer;
+}
