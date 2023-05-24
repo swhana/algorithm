c184f98abba218edc1901bf607a75f175442c0c9
Han Sang Won <109811519+swhana@users.noreply.github.com>
2023-05-24 13:02:55 +0900

    Update 겹치는 건 싫어.java

--- 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.java
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.java
@@ -1,33 +1,39 @@
+//원래 풀이도 방법은 맞았는데 HashMap을 Array로 바꾸니 해결되서 아쉬워서 올린 코드. HashMap이라고 무조건 빠른 것은 아닌것같다
+
 import java.util.*;
 
-public class Main {
+public class Solution_BOJ_겹치는건싫어_sangwon {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
+		
 		int N = sc.nextInt();
 		int K = sc.nextInt();
-
-		int[] arr = new int[200000];
+		
 		int[] count = new int[100001];
-
+		int[] arr = new int[N];
+		
 		int max = Integer.MIN_VALUE;
 		int lp = 0;
 		int rp = 0;
 
-		for (int i = 0; i < N; i++)
+		for(int i=0; i<N; i++) {
 			arr[i] = sc.nextInt();
-
-		while (rp < N) {
-			if (count[arr[rp]] == K) {
-				count[arr[lp]]--; // 연속 부분수열의 맨 왼쪽 원소부터 제외
-				lp++;
-			} else {
-				count[arr[rp]]++;
-				rp++;
+			int cnt = count[arr[i]];
+			if(cnt == K) {
+				while(lp < rp) {
+					count[arr[lp]]--; //연속 부분수열의 맨 왼쪽 원소부터 제외
+					if(arr[lp] == arr[i]) {
+						lp++;
+						break;
+					}
+					lp++;
+				}
 			}
-			max = Math.max(max, rp - lp);
-		}
+			count[arr[i]]++;
+			rp++;
+			if(max < rp-lp) max = rp-lp;
+		}	
 
 		System.out.println(max);
 	}
