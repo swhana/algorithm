363952509f4bd027d28bf369f1e74559944359af
Han Sang Won <109811519+swhana@users.noreply.github.com>
2023-06-29 17:36:45 +0900

    [Gold IV] Title: 서강그라운드, Time: 140 ms, Memory: 13408 KB -BaekjoonHub

---
+++ 백준/Gold/14938. 서강그라운드/서강그라운드.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+public class Main {
+	static final int INF = (int) 1e9;
+	static int n, m, r;
+	static int[][] dist;
+	static int[] item;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		n = sc.nextInt();
+		m = sc.nextInt();
+		r = sc.nextInt();
+		
+		dist = new int[n+1][n+1];
+		item = new int[n+1];
+		
+		for(int i=1; i<n+1; i++) {
+			Arrays.fill(dist[i], INF);
+			dist[i][i] = 0;
+		}
+		
+		for(int i=1; i<n+1; i++) item[i] = sc.nextInt();
+		
+		for(int i=0; i<r; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int l = sc.nextInt();
+			
+			dist[a][b] = Math.min(dist[a][b], l);
+			dist[b][a] = Math.min(dist[b][a], l);
+		}
+		
+		floyd();
+		
+		
+		int maxItem = 0;
+		for(int s=1; s<n+1; s++) {
+			int getItem = 0;
+			for(int e=1; e<n+1; e++) {
+				if(dist[s][e] <= m) getItem += item[e];
+			}
+			
+			maxItem = Math.max(maxItem, getItem);
+		}
+		
+		System.out.println(maxItem);
+	}
+
+	static void floyd() {
+		for(int w=1; w<n+1; w++) {
+			for(int s=1; s<n+1; s++) {
+				if(s == w || dist[s][w] == INF) continue;
+				for(int e=1; e<n+1; e++) {
+					if(w == e || dist[w][e] == INF) continue;
+					dist[s][e] = Math.min(dist[s][e], dist[s][w] + dist[w][e]);
+				}
+			}
+		}
+	}
+}
