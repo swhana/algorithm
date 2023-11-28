d6f767eaee3788a9a87db08da7912cb3039d1616
Han Sang Won <109811519+swhana@users.noreply.github.com>
2023-11-28 19:26:35 +0900

    [level 1] Title: 최소직사각형, Time: 6.67 ms, Memory: 40.3 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -1,18 +1,12 @@
 function solution(sizes) {
     var answer = 0;
     
-    sizes.forEach(size => {
-        if(size[0] < size[1]) {
-            let temp = size[1];
-            size[1] = size[0];
-            size[0] = temp;
-        }
-    })
+    const resized = sizes.map(([w, h]) => w < h ? [h, w] : [w, h])
     
     let maxW = 0;
     let maxH = 0;
     
-    sizes.forEach(([w, h]) => {
+    resized.forEach(([w, h]) => {
         maxW = Math.max(w, maxW);
         maxH = Math.max(h, maxH);
     })
